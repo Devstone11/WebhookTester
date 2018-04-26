7afa69db6f417ce6a1325cb1cfc6ddb27d3ed028
mschwa <kris.boyce@orthofi.com>
2018-04-26 15:12:55 -0600

    make a really good change

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,6 @@ namespace WebHookTester
         {
             services.AddMvc();
             services.AddRouting();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
